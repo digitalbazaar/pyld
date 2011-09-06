5d4323a92b99aa7422bddadf32ccefac2ad8480a
David I. Lehn <dlehn@digitalbazaar.com>
2011-09-05 20:21:05 -0400

    Add basic setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+pyld
+====
+`pyld`_ is a Python `JSON-LD` library.
+
+.. _pyld: http://github.com/digitalbazaar/pyld
+.. _JSON-LD: http://json-ld.org/
+"""
+from setuptools import setup
+
+setup(
+    name = 'pyld',
+    version = '0.0.1',
+    license = 'BSD 3-Clause license',
+    url = 'http://github.com/digitalbazaar/pyld',
+    author = 'Digital Bazaar',
+    author_email = 'support@digitalbazaar.com',
+    playforms = 'any',
+    py_modules = [
+        'pyld',
+    ],
+    classifiers = [
+    ]
+)
