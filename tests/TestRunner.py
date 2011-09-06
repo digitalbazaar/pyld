59cc33532cc1a8455bfda31ee1b0be05e41c2011
David I. Lehn <dlehn@digitalbazaar.com>
2011-09-05 20:06:02 -0400

    Update testing docs and pyld path.

--- tests/TestRunner.py
+++ tests/TestRunner.py
@@ -10,7 +10,7 @@ import os, sys, json
 from os.path import join
 from optparse import OptionParser
 
-sys.path += ('../lib',)
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 from pyld import jsonld
 
 ##
