e367ba418ac2142cc5c9d0a9350716a2946d1bca
Dave Longley <dlongley@digitalbazaar.com>
2013-09-19 16:35:20 -0400

    Bump version.

--- lib/pyld/jsonld.py
+++ lib/pyld/jsonld.py
@@ -14,7 +14,7 @@ JSON-LD.
 
 __copyright__ = 'Copyright (c) 2011-2013 Digital Bazaar, Inc.'
 __license__ = 'New BSD license'
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 __all__ = ['compact', 'expand', 'flatten', 'frame', 'from_rdf', 'to_rdf',
     'normalize', 'set_document_loader', 'get_document_loader',
