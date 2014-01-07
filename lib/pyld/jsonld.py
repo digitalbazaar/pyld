159bbc11bb893f2d50b453136738e5b59a80e281
Dave Longley <dlongley@digitalbazaar.com>
2014-01-07 11:00:03 -0500

    Start 0.4.11.

--- lib/pyld/jsonld.py
+++ lib/pyld/jsonld.py
@@ -14,7 +14,7 @@ JSON-LD.
 
 __copyright__ = 'Copyright (c) 2011-2013 Digital Bazaar, Inc.'
 __license__ = 'New BSD license'
-__version__ = '0.4.10'
+__version__ = '0.4.11-dev'
 
 __all__ = [
     'compact', 'expand', 'flatten', 'frame', 'from_rdf', 'to_rdf',
