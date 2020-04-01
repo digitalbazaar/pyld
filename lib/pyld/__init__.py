932bbe0ba5e1461e4bcb26a807c5b163938d4ea8
Gregg Kellogg <gregg@greggkellogg.net>
2020-03-31 17:00:37 -0700

    Add some initialization for ContextResolver to pyld/__init__.py

--- lib/pyld/__init__.py
+++ lib/pyld/__init__.py
@@ -1,4 +1,5 @@
 """ The PyLD module is used to process JSON-LD. """
 from . import jsonld
+from .context_resolver import ContextResolver
 
-__all__ = ['jsonld']
+__all__ = ['jsonld', 'ContextResolver']
