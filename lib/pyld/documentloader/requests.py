21352fdada4c1780687222f600b77644ca59e7e7
Christian Gils <cgils@google.com>
2020-07-16 15:42:16 -0700

    Adds headers to kwargs documentloader.requests.loader(). Fixes issue #125.

--- lib/pyld/documentloader/requests.py
+++ lib/pyld/documentloader/requests.py
@@ -60,7 +60,8 @@ def requests_document_loader(secure=False, **kwargs):
                 headers = {
                     'Accept': 'application/ld+json, application/json'
                 }
-            response = requests.get(url, headers=headers, **kwargs)
+                kwargs['headers'] = headers
+            response = requests.get(url, **kwargs)
 
             content_type = response.headers.get('content-type')
             if not content_type:
