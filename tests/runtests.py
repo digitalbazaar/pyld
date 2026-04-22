e91f7e07a338f1d209479e43b1d803caea11e9ea
Miel Vander Sande <miel.vandersande@meemoo.be>
2026-04-22 15:00:21 +0200

    Add configurable hashAlgorithm

--- tests/runtests.py
+++ tests/runtests.py
@@ -679,6 +679,8 @@ def create_test_options(opts=None):
             if k not in http_options:
                 options[k] = v
         options['documentLoader'] = create_document_loader(test)
+        # options['hashAlgorithm'] = test.data.get('hashAlgorithm')
+
         if 'expandContext' in options:
             filename = os.path.join(test.dirname, options['expandContext'])
             options['expandContext'] = read_json(filename)
