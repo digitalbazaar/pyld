0686743f1e8fdc868589bc344b91b3eb1158f639
Miel Vander Sande <miel.vandersande@meemoo.be>
2026-05-13 12:09:56 +0200

    Move toRdf tests into test_jsonld for now.

--- tests/test_jsonld.py
+++ tests/test_jsonld.py
@@ -780,6 +780,59 @@ class TestToRdf:
         result = jsonld.to_rdf(input)
         assert result == expected
 
+     # Issue 204
+    def test_conflicting_property_names(self):
+        """
+        Conversion to RDF should allow a node in the root @context with
+        a conflicting property name in its own @context
+        """
+        input = {
+            "@context": {
+                "dublinCore": {
+                    "@id": "http://foo.bar/dc",
+                    "@context": {"title": "http://purl.org/dc/terms/title"},
+                },
+                "title": "http://foo.bar/title",
+            },
+            "@id": "http://foo.bar/obj/test",
+            "title": "test",
+            "dublinCore": {"title": "Chapter 1: Jonathan Harker's Journal"},
+        }
+
+        expected = """<http://foo.bar/obj/test> <http://foo.bar/dc> _:b0 .
+<http://foo.bar/obj/test> <http://foo.bar/title> "test" .
+_:b0 <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal" .
+"""
+
+        nquads = jsonld.to_rdf(input, options={'format': 'application/n-quads'})
+        assert nquads == expected
+
+
+    def test_conflicting_property_names_in_nested_node(self):
+        """
+        Conversion to RDF should not ignore a @nest'ed node in the root @context
+        a conflicting property name in its own @context
+        """
+        input = {
+            "@context": {
+                "dublinCore": {
+                    "@id": "@nest",
+                    "@context": {"title": "http://purl.org/dc/terms/title"},
+                },
+                "title": "http://foo.bar/title",
+            },
+            "@id": "http://foo.bar/obj/test",
+            "title": "test",
+            "dublinCore": {"title": "Chapter 1: Jonathan Harker's Journal"},
+        }
+
+        expected = """<http://foo.bar/obj/test> <http://foo.bar/title> "test" .
+<http://foo.bar/obj/test> <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal" .
+"""
+
+        nquads = jsonld.to_rdf(input, options={'format': 'application/n-quads'})
+        assert nquads == expected
+
 
 class TestCompact:
     # Issue 59 - PR: https://github.com/digitalbazaar/pyld/pull/60
