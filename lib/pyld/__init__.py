e5ec62a63216d4d63fef5df9d81f10c99d2c66a8
Anatoly Scherbakov <altaisoft@gmail.com>
2026-05-18 16:55:14 +0300

    [#261]: Export built-in document loader classes

--- lib/pyld/__init__.py
+++ lib/pyld/__init__.py
@@ -2,14 +2,18 @@
 
 from . import jsonld
 from .context_resolver import ContextResolver
+from .documentloader.aiohttp import AioHttpDocumentLoader
 from .documentloader.base import DocumentLoader, RemoteDocument
 from .documentloader.frozen import BUNDLED_CONTEXTS, FrozenDocumentLoader
+from .documentloader.requests import RequestsDocumentLoader
 
 __all__ = [
+    'AioHttpDocumentLoader',
     'BUNDLED_CONTEXTS',
     'ContextResolver',
     'DocumentLoader',
     'FrozenDocumentLoader',
+    'RequestsDocumentLoader',
     'RemoteDocument',
     'jsonld',
 ]
