9a8524db64c3bccfcead49001facf44e255500fc
Miel Vander Sande <miel.vandersande@meemoo.be>
2026-05-21 10:30:27 +0200

    Adjust tests nquad output

--- tests/test_jsonld.py
+++ tests/test_jsonld.py
@@ -947,13 +947,15 @@ _:b1 <http://www.w3.org/1999/02/22-rdf-syntax-ns#value> "en-US" .
             "dublinCore": {"title": "Chapter 1: Jonathan Harker's Journal"},
         }
 
-        expected = """<http://foo.bar/obj/test> <http://foo.bar/dc> _:b0 .
-<http://foo.bar/obj/test> <http://foo.bar/title> "test" .
-_:b0 <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal" .
+        expected = """<http://foo.bar/obj/test> <http://foo.bar/title> "test"^^<http://www.w3.org/2001/XMLSchema#string>  .
+<http://foo.bar/obj/test> <http://foo.bar/dc> _:b0  .
+_:b0 <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal"^^<http://www.w3.org/2001/XMLSchema#string>  .
+
 """
 
         nquads = jsonld.to_rdf(input, options={'format': 'application/n-quads'})
-        assert nquads == expected
+        # TODO: move this into a helper function for comparing nquads
+        assert sorted(nquads.splitlines()) == sorted(expected.splitlines())
 
 
     def test_conflicting_property_names_in_nested_node(self):
@@ -974,12 +976,14 @@ _:b0 <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal" .
             "dublinCore": {"title": "Chapter 1: Jonathan Harker's Journal"},
         }
 
-        expected = """<http://foo.bar/obj/test> <http://foo.bar/title> "test" .
-<http://foo.bar/obj/test> <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal" .
+        expected = """<http://foo.bar/obj/test> <http://foo.bar/title> "test"^^<http://www.w3.org/2001/XMLSchema#string>  .
+<http://foo.bar/obj/test> <http://purl.org/dc/terms/title> "Chapter 1: Jonathan Harker's Journal"^^<http://www.w3.org/2001/XMLSchema#string>  .
+
 """
 
         nquads = jsonld.to_rdf(input, options={'format': 'application/n-quads'})
-        assert nquads == expected
+        # TODO: move this into a helper function for comparing nquads
+        assert sorted(nquads.splitlines()) == sorted(expected.splitlines())
 
 class TestFromRDF:
     def test_compound_literal_direction_without_language(self):
