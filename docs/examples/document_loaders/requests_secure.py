58676616e382ed62b56e119f9e55abf5db8edf8d
Anatoly Scherbakov <altaisoft@gmail.com>
2026-06-13 23:48:27 +0400

    Add secure RequestsDocumentLoader example

---
+++ docs/examples/document_loaders/requests_secure.py
@@ -0,0 +1,12 @@
+import json
+
+from pyld import RequestsDocumentLoader, jsonld
+
+doc = {
+    "@context": {"name": "http://schema.org/name"},
+    "name": "Earth",
+}
+
+loader = RequestsDocumentLoader(secure=True, timeout=10)
+result = jsonld.expand(doc, options={"documentLoader": loader})
+print(json.dumps(result, indent=2))
