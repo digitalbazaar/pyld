b2d607e414694a8ec2d877cf0b5422ce71c621bf
Miel Vander Sande <miel.vandersande@meemoo.be>
2026-09-07 11:07:26 +0200

    Add tests for max permutations

--- tests/test_jsonld.py
+++ tests/test_jsonld.py
@@ -968,6 +968,28 @@ class TestFlatten:
 
 
 class TestNormalize:
+    def test_normalize_max_canonicalization_permutations_raises(self):
+        """
+        Normalization raises when the RDFC10 permutation budget is exhausted.
+        """
+        input = (
+            '_:a <http://example.org/p> _:b .\n'
+            '_:a <http://example.org/p> _:c .\n'
+        )
+
+        with pytest.raises(jsonld.JsonLdError) as exc:
+            jsonld.normalize(
+                input,
+                options={
+                    'algorithm': 'RDFC10',
+                    'inputFormat': 'application/n-quads',
+                    'format': 'application/n-quads',
+                    'maxPermutations': 0,
+                },
+            )
+
+        assert exc.value.code == 'maximum canonicalization work exceeded'
+
     def test_normalize_does_not_pass_identifier_issuer_to_to_rdf(self):
         class RaisingIdentifierIssuer(IdentifierIssuer):
             def get_id(self, old=None):
