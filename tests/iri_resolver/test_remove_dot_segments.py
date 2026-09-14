d8d1c64f05a8612c2f104080da3fc8fb17a97bc3
Anatoly Scherbakov <altaisoft@gmail.com>
2026-09-15 00:19:04 +0400

    Update tests/iri_resolver/test_remove_dot_segments.py
    Co-authored-by: Miel Vander Sande <miel.vandersande@meemoo.be>

--- tests/iri_resolver/test_remove_dot_segments.py
+++ tests/iri_resolver/test_remove_dot_segments.py
@@ -1,7 +1,7 @@
 from pyld.iri_resolver import remove_dot_segments
 
 # Tests ported from relative-to-absolute-iri.js: https://github.com/rubensworks/relative-to-absolute-iri.js/blob/master/test/Resolve-test.ts
-# (c) Ruben Taelman <stevenlevithan.com>
+# (c) Ruben Taelman <https://www.rubensworks.net>
 
 
 # ---------- Tests for remove_dot_segments() ----------
