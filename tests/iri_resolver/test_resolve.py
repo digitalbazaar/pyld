b75e2d7fe4a02d6833d20431369695b5b67eb5c1
Anatoly Scherbakov <altaisoft@gmail.com>
2026-09-15 00:18:39 +0400

    Update tests/iri_resolver/test_resolve.py
    Co-authored-by: Miel Vander Sande <miel.vandersande@meemoo.be>

--- tests/iri_resolver/test_resolve.py
+++ tests/iri_resolver/test_resolve.py
@@ -3,7 +3,7 @@ import pytest
 from pyld.iri_resolver import resolve
 
 # Tests ported from relative-to-absolute-iri.js: https://github.com/rubensworks/relative-to-absolute-iri.js/blob/master/test/Resolve-test.ts
-# (c) Ruben Taelman <stevenlevithan.com>
+# (c) Ruben Taelman <https://www.rubensworks.net>
 
 
 # ---------- Tests for resolve() ----------
